40327f1cc1e368b2eea0310f0a971b10a0bab76c
Terry Lee <slsk0219@gmail.com>
2021-09-02 15:07:34 -0700

    additional webpack plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "./styles.css"
+
 export const App = () => {
-  return <h1>React Typescript Webpack Boilerplate</h1>
+  return <h1>React Typescript Webpack Boilerplate {process.env.NODE_ENV}</h1>
 }
